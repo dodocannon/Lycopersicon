57b1eb3714da4898d42d04d5455726810a214fbd
Kevin <kevin12kim27@gmail.com>
2019-01-24 21:19:24 -0800

    deleted screens, final working versions until I implement texture packing. too lazy till then

--- core/src/com/mygdx/game/Lycopersicon.java
+++ core/src/com/mygdx/game/Lycopersicon.java
@@ -2,17 +2,10 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.mygdx.game.Screens.IntroScreen;
+
 import com.mygdx.game.Screens.LycopersiconScreen;
 
 public class Lycopersicon extends Game {
-	private IntroScreen tIntroScreen;
-	private LycopersiconScreen tGameScreen;
-	private Game game;
 
 
 	@Override
@@ -25,12 +18,7 @@ public class Lycopersicon extends Game {
 	@Override
 	public void render () {
 /*
-		if (getScreen().equals(tIntroScreen) && tIntroScreen.tElapsedTime > 2.5)
-		{
-			tIntroScreen.dispose();
 
-			setScreen(tGameScreen);
-		}
 */
 		super.render();
 	}
